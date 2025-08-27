d568e6a81fb86b5e428380972306e79b0817040f
Jared Wellman <jwellman@gmail.com>
2025-08-27 12:51:04 -0500

    Autocommit from 2025-08-27 12:51:04
    Klipper version: v0.13.0-181-ge1176e4df
    
    Moonraker version: v0.9.3-102-gf20feff
    
    Mainsail version: v2.14.0

--- autocommit.sh
+++ autocommit.sh
@@ -32,7 +32,7 @@ mainsail_folder=~/mainsail
 
 ### The branch of the repository that you want to save your config
 ### By default that is 'main'
-branch=stealthchanger-alpha
+branch=v-repo-scanner
 
 db_file=~/printer_data/database/moonraker-sql.db
 
